25143593f6aae66c8a30939cca14e467cfbd4d70
user <user@owners-MacBook-Air.local>
2026-04-22 18:44:43 +0200

    Create project, add modules

---
+++ energy-community/gui-app/src/main/java/com/energy/community/guiapp/HelloController.java
@@ -0,0 +1,14 @@
+package com.energy.community.guiapp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ energy-community/gui-app/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.energy.community.guiapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.energy.community.guiapp to javafx.fxml;
+    exports com.energy.community.guiapp;
+}
