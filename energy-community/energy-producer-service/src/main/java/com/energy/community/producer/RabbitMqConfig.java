3ac056f5c8e347197ff7a0a8ccefb296ba2b669e
niederwieserj <github.spectacle840@passmail.com>
2026-06-22 14:47:10 +0200

    Add config, scheduled method to send producedKwh

---
+++ energy-community/energy-producer-service/src/main/java/com/energy/community/producer/RabbitMqConfig.java
@@ -0,0 +1,43 @@
+package com.energy.community.producer;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+
+@Configuration
+public class RabbitMqConfig {
+
+    public static final String PRODUCED_KWH_QUEUE = "producedKwh";
+    public static final String RABBITMQ_USERNAME = "guest";
+    public static final String RABBITMQ_PASSWORD = "guest";
+
+    @Bean
+    public Queue producedKWh() {
+        return new Queue(PRODUCED_KWH_QUEUE, false);
+    }
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
+        connectionFactory.setUsername(RABBITMQ_USERNAME);
+        connectionFactory.setPassword(RABBITMQ_PASSWORD);
+        return connectionFactory;
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new JacksonJsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
+    }
+}
---
+++ energy-community/energy-producer-service/src/main/java/com/energy/community/producer/Worker.java
@@ -0,0 +1,22 @@
+package com.energy.community.producer;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class Worker {
+    private final RabbitTemplate rabbitTemplate;
+
+    public Worker(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Scheduled(fixedRate = 10000)
+    public void Work() {
+        ProducedKwhDto producedKwhDto = new ProducedKwhDto("PRODUCER", "COMMUNITY", 0.0023, LocalDateTime.now());
+        rabbitTemplate.convertAndSend(RabbitMqConfig.PRODUCED_KWH_QUEUE, producedKwhDto);
+    }
+}
