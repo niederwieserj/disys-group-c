9e0d3836774c60d03d62b4a0a92cc314a0588cf2
niederwieserj <github.spectacle840@passmail.com>
2026-06-29 11:45:34 +0200

    Rename and structure producer

--- energy-community/energy-producer-service/src/main/java/com/energy/community/producer/config/RabbitMqConfig.java
+++ energy-community/energy-producer-service/src/main/java/com/energy/community/producer/config/RabbitMqConfig.java
@@ -1,4 +1,4 @@
-package com.energy.community.producer;
+package com.energy.community.producer.config;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
